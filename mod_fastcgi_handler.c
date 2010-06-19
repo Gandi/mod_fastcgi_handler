41d10bf4cbb24daff2e32d9185d062c8eeaa6dd9
Benedikt Böhm <bb@xnull.de>
2010-06-19 18:18:02 +0200

    all headers are handled by ap_add_common_vars, no need for FastCgiPassHeaders

--- mod_fastcgi_handler.c
+++ mod_fastcgi_handler.c
@@ -4,7 +4,7 @@
 #include "fcgi_server.h"
 
 static
-int fastcgi_handler_handler(request_rec *r)
+int fastcgi_handler(request_rec *r)
 {
 	if (strncmp(r->handler, "fcgi:", 5))
 		return DECLINED;
@@ -32,7 +32,6 @@ void *fastcgi_handler_create_dir_config(apr_pool_t *p, char *dir)
 	fastcgi_handler_cfg *cfg = apr_pcalloc(p, sizeof(fastcgi_handler_cfg));
 
 	cfg->idle_timeout = -1;
-	cfg->headers = apr_array_make(p, 1, sizeof(char *));
 
 	return cfg;
 }
@@ -47,33 +46,19 @@ void *fastcgi_handler_merge_dir_config(apr_pool_t *p, void *parent, void *curren
 	cfg->idle_timeout = current_cfg->idle_timeout == -1 ?
 			parent_cfg->idle_timeout : current_cfg->idle_timeout;
 
-	cfg->headers = apr_array_append(p, parent_cfg->headers, current_cfg->headers);
-
 	return cfg;
-}
-
-static
-const char *fastcgi_handler_cmd_pass_header(cmd_parms *cmd, void *mconf,
-		const char *arg)
-{
-	fastcgi_handler_cfg *cfg = (fastcgi_handler_cfg *) mconf;
-	*(const char **)apr_array_push(cfg->headers) = arg;
-	return NULL;
 }
 
 static
 const command_rec fastcgi_handler_cmds[] =
 {
-	AP_INIT_ITERATE("FastCgiPassHeader", fastcgi_handler_cmd_pass_header, NULL,
-			OR_FILEINFO, "a list of headers to pass to the FastCGI application."),
-
 	{ NULL }
 };
 
 static
 void fastcgi_handler_register_hooks(apr_pool_t * p)
 {
-	ap_hook_handler(fastcgi_handler_handler, NULL, NULL, APR_HOOK_MIDDLE);
+	ap_hook_handler(fastcgi_handler, NULL, NULL, APR_HOOK_MIDDLE);
 }
 
 module AP_MODULE_DECLARE_DATA fastcgi_handler_module =
